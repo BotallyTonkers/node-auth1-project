70f18afb5365ab80d790e230807610f8dd9dbe6e
Amanda Young <botallytonkers@gmail.com>
2023-11-24 01:58:24 -0700

    login

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -61,7 +61,13 @@ checkUsernameFree,
  */
   // eslint-disable-next-line no-unused-vars
   router.post('/login', checkUsernameExists, (req, res, next) => {
-    res.json('login')
+    const { password } = req.body
+    if (bcrypt.compareSync(password, req.user.password)) {
+      req.session.user = req.user
+      res.json({ message: `Welcome ${req.user.username}` })
+    } else {
+      next({ status: 401, message: 'Invalid credentials' })
+    }
   })
 
 /**
